fb13f22240a822212db30ddfa55a8c9dd10ccfac
MikyViz <vizenovsky@gmail.com>
2026-05-12 23:45:52 +0300

    fix: fix case-sensitive import path for reviewControler on Linux

--- aboutmebe/routers/reviewRouter.js
+++ aboutmebe/routers/reviewRouter.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import ReviewController from '../controllers/ReviewControler.js';
+import ReviewController from '../controllers/reviewControler.js';
 import auth from '../middlewares/auth.js';
 const router = express.Router();
 
